bd9defdd7daa915541ae445bb0db57921fdc15e7
Daniel Quintero <quinterod164@gmail.com>
2026-05-23 19:46:29 -0500

    feat: agregar comentarios explicativos sobre GoLiveDate en SICRESettings

--- Sicre.Api/Domain/Entities/SICRESettings.cs
+++ Sicre.Api/Domain/Entities/SICRESettings.cs
@@ -3,6 +3,10 @@ namespace Sicre.Api.Domain.Entities;
 public class SICRESettings
 {
     public Guid Id { get; set; }
+
+    // Fecha desde la cual SICRE empieza a controlar y generar instancias de reportes.
+    // No se deben generar vencimientos anteriores a esta fecha.
+    // Se empiezan a generar las obligaciones por primera vez al mes siguiente a esta fecha.
     public DateOnly? GoLiveDate { get; set; }
     public bool AutoNotify { get; set; } = true;
     public Guid? UpdatedByUserId { get; set; }
