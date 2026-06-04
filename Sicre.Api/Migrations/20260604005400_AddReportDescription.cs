1d2ca27403854ca79572e657c51d3ff913058cc0
Daniel Quintero <quinterod164@gmail.com>
2026-06-03 20:04:53 -0500

    feat: migracion AddReportDescription

---
+++ Sicre.Api/Migrations/20260604005400_AddReportDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sicre.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "reports",
+                table: "reports",
+                type: "text",
+                nullable: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "Description", schema: "reports", table: "reports");
+        }
+    }
+}
